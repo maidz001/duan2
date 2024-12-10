2e8016902c912065ac201676033e949964860e6c
maidz <maipro8a4@gmail.com>
2024-12-10 23:14:29 +0700

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const messages = [
-  "Nhớ nhớ nhớ em!"
-];
+const messages = ["Nhớ nhớ nhớ em!"];
 
 function generateRandomNotifications() {
   const notificationCount = 100;
 
   for (let i = 0; i < notificationCount; i++) {
     setTimeout(() => {
-      const notification = document.createElement('div');
-      notification.className = 'notification';
+      const notification = document.createElement("div");
+      notification.className = "notification";
 
-      const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+      const randomMessage =
+        messages[Math.floor(Math.random() * messages.length)];
       notification.innerHTML = `
         <div class="notification-header">
           <button class="minimize-btn" onclick="minimizeNotification(this)">–</button>
-          <span>Tràn ngập bộ</span>
+          <span>Tràn ngập bộ nhớ</span>
         </div>
         <p>${randomMessage}</p>
       `;
@@ -30,13 +29,14 @@ function generateRandomNotifications() {
 }
 
 function minimizeNotification(button) {
-  const notification = button.closest('.notification');
-  notification.style.display = 'none'; 
+  const notification = button.closest(".notification");
+  notification.style.display = "none";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var encodedText = '&#68;&#101;&#115;&#105;&#103;&#110;&#32;&#98;&#121;&#32;&#80;&#97;&#110;&#98;&#97;&#112;';
+  var encodedText =
+    "&#68;&#101;&#115;&#105;&#103;&#110;&#32;&#98;&#121;&#32;&#80;&#97;&#110;&#98;&#97;&#112;";
   var footer = document.createElement("a");
-  footer.innerHTML = encodedText; 
-  document.body.appendChild(footer); 
+  footer.innerHTML = encodedText;
+  document.body.appendChild(footer);
 });
